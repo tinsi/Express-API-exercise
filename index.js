4e3aaba806e2d3b10097430606b10ea16f6ae972
tiinaVaisanen <tiina.vaisanen@awacademy.training>
2022-07-09 14:38:46 +0300

    ...

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const db = require("./services/user-service");
 const port = 3000;
+const logger = require("morgan");
 
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -21,6 +23,24 @@ app.post("/users", db.createUser);
 app.put("/users/:id", db.updateUser);
 app.delete("/users/:id", db.deleteUser);
 
+//Express-generaattorista napattu:
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+// Tää oli jo alunperin
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
 });
